d914eeb2ad779168b2fd1739ecfc7c126e67abbd
legalcheng23 <legalcheng23@gmail.com>
2024-06-06 17:35:56 +0800

    init commit

--- code/puf.py
+++ code/puf.py
@@ -1,32 +1,32 @@
-import serial # 引用pySerial模組
+import serial 
 import hashlib
-COM_PORT = 'COM4'    # 指定通訊埠名稱
-BAUD_RATES = 9600    # 設定傳輸速率
-ser = serial.Serial(COM_PORT, BAUD_RATES)   # 初始化序列通訊埠
+COM_PORT = 'COM4'    
+BAUD_RATES = 9600    
+ser = serial.Serial(COM_PORT, BAUD_RATES)  
 ser1=serial.Serial('COM3',9600)
 def UID():
     try:
         while True:
-            while ser.in_waiting:          # 若收到序列資料…
-                data_raw = ser.readline()  # 讀取一行
-                data = data_raw.decode()   # 用預設的UTF-8解碼
-                print('接收到的資料：', data)
+            while ser.in_waiting:        
+                data_raw = ser.readline()  
+                data = data_raw.decode()   
+                print('Information Received：', data)
                 return data
     except ValueError:
-        ser.close()    # 清除序列通訊物件
-        print('再見！')
+        ser.close()   
+        print('bye！')
 
 def PUF():
     try:
         while True:
-            while ser1.in_waiting:          # 若收到序列資料…
+            while ser1.in_waiting:        
                 data_raw1=ser1.readline()
                 data1=data_raw1.decode()
-                print('接收到的資料：', data1)
+                print('Information Received：', data1)
                 return data1
     except ValueError:
-        ser1.close()    # 清除序列通訊物件
-        print('再見！')
+        ser1.close() 
+        print('bye！')
 
 def hashpuf(Puf,candidate):
     BinaryString = ''
